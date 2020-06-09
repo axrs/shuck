048574538d0bdd9ffdf2713b7718011f6898e4c6
Alexander Scott <xander@axrs.io>
2020-06-09 19:45:23 +1000

    Change example

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject io.axrs/shuck "0.0.0-SNAPSHOT"
+  :description "Shell Executor for Clojure"
+  :min-lein-version "2.8.1"
+  :dependencies []
+  :clean-targets ["target"]
+  :source-paths ["src"]
+  :test-paths ["test"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}})
---
+++ src/io/axrs/shuck/core.clj
@@ -0,0 +1,54 @@
+(ns io.axrs.shuck.core
+  (:require
+    [clojure.java.io :as io]
+    [clojure.string :as str]))
+
+(def ^:dynamic *print-out* false)
+(def ^:dynamic *print-err* false)
+(def ^:private -lock)
+
+(defn- sync-println [& more]
+  (locking -lock (apply println more)))
+
+;TODO sh! which throws run-time if cli fails
+(defn sh
+  "Executes shell command, capturing output, duration and exit-codes.
+  Note: Remember to call `io.axrs.shuck.core/done` before process exit to cleanup any running agents"
+  [& args]
+  (let [start (System/currentTimeMillis)
+        printerr (if *print-err*
+                   (fn [& more]
+                     (sync-println (str (char 27) "[31m" (apply str more) (char 27) "[0m")))
+                   (constantly nil))
+        printout (if *print-out* sync-println (constantly nil))
+        p-builder (ProcessBuilder. args)
+        process (.start p-builder)
+        err (transient [])
+        output (transient [])
+        consume (fn [t-col print input-stream]
+                  (future (with-open [output-reader (io/reader input-stream)]
+                            (loop []
+                              (when-let [line (.readLine output-reader)]
+                                (print line)
+                                (conj! t-col line)
+                                (recur))))))
+        f1 (consume output printout (.getInputStream process))
+        f2 (consume err printerr (.getErrorStream process))]
+    (.waitFor process)
+    @f1
+    @f2
+    {:exit     (.exitValue process)
+     :duration (- (System/currentTimeMillis) start)
+     :err      (str/join \newline (persistent! err))
+     :out      (str/join \newline (persistent! output))}))
+
+(defn done []
+  (shutdown-agents))
+
+(comment
+  (binding [*print-out* true
+            *print-err* true]
+    (-> (sh "npm" "install")
+        :out
+        (clojure.pprint/pprint))
+    (done)))
